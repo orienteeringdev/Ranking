8ba3876ddb4a919c8256ef5845c893996850c19a
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-12-19 19:19:57 +0000

    .

--- data/collect.py
+++ data/collect.py
@@ -5,7 +5,9 @@ ARCHIVE_CALENDAR_URL='http://archives.cn.ffcorientation.fr/calendrier.php'
 
 def parse_archive_calendar(tree):
     # brute force : get all tr - check if td with link to competition.php inside
-    tree.find_attribute
+    for tr in tree.find_element("tr"):
+        td = tr.find_element("td")
+        if
 
 def collect_archive():
     calendar = html.fromstring(requests.get(ARCHIVE_CALENDAR_URL).content)
